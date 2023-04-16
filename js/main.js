50012b6d768bf81ce7f2ad8e2c9fccac2c8de536
Somayaznaid <126412030+Somayaznaid@users.noreply.github.com>
2023-04-17 01:34:28 +0300

    commit_last_7

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 let log_text = document.getElementById("log_text_btn");
 let if_dont_log = document.getElementById("if_dont_log");
 let html_butn = document.getElementById("html_butn");
+let css_butn = document.getElementById("css_butn");
+let js_butn = document.getElementById("js_butn");
+
 
 
 html_butn.addEventListener("click", function (e) {
@@ -11,13 +14,46 @@ html_butn.addEventListener("click", function (e) {
      if(currentSessionObj === ""){
         window.location.href = "sign-up.html";
      } else {
-    // console.log(currentSessionObj);
+    
     window.location.href = "qiuz.html";
      }
 });
 
+
+css_butn.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    let currentSessionObj = window.sessionStorage.getItem("users_log_1");
+    
+     if(currentSessionObj === ""){
+        window.location.href = "sign-up.html";
+     } else {
+    
+    window.location.href = "qiuz - css.html";
+     }
+});
+
+
+js_butn.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    let currentSession = window.sessionStorage.getItem("users_log_1");
+    let currentSessionObj = JSON.parse(currentSession);
+    
+     if(currentSessionObj === null){
+        window.location.href = "sign-up.html";
+     } else if (currentSessionObj !== null) {
+    
+    window.location.href = "qiuz - js.html";
+     }
+});
+
+
+
 log_text.addEventListener("click", function () {
     window.sessionStorage.clear();
+    if_dont_log.textContent = "plz log in to get the Quizez";
+
 });
 
 
